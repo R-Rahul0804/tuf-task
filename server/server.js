4ebae7a0ce03c6f6d214f512e15a56be92deb1f0
R-Rahul0804 <rahul08042001@gmail.com>
2024-03-20 19:21:39 +0530

    sixth commit

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv'); dotenv.config();
 const db = require('./config/sqlConnection');
@@ -12,6 +13,15 @@ app.use(express.urlencoded({extended:true}));
 
 
 app.use('/api', userCodeRoute);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../client/build')));
+    app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'../client','build','index.html')));
+}else{
+    app.get('/', (req,res)=>{
+        res.send('API is running....');
+    });
+}
 
 app.listen(PORT,()=>{
     console.log(`Server is Running on ${PORT}`);
